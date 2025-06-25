d9257cd5e1634a528ce9bffa9382ebbe6ced34d9
GODWIN-projects <167623701+GODWIN-projects@users.noreply.github.com>
2025-06-25 17:58:03 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,9 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+
 export const metadata = {
   metadataBase: new URL("https://ai-finance-pied.vercel.app"),
   title: "Smart Finance",
